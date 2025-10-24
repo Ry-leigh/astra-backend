6227da62fd9ea0965fdd62b71b0ff0bb0c7d65fe
Ry-leigh <ryanaarondc@gmail.com>
2025-10-24 22:03:20 +0800

    Migrations: updated announcements table
    - add last_updated_by column for updating
    - changed cascadeOnDelete() to nullOnDelete() in created_by column

--- database/migrations/0001_01_01_000013_create_announcements_table.php
+++ database/migrations/0001_01_01_000013_create_announcements_table.php
@@ -10,11 +10,12 @@ return new class extends Migration
     {
         Schema::create('announcements', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('created_by')->constrained('users')->cascadeOnDelete();
+            $table->foreignId('created_by')->nullable()->constrained('users')->nullOnDelete();
             $table->string('title');
             $table->text('description')->nullable();
             $table->date('event_date')->nullable();
             $table->time('event_time')->nullable();
+            $table->foreignId('last_updated_by')->nullable()->default(null)->constrained('users')->nullOnDelete();
             $table->timestamps();
         });
     }
