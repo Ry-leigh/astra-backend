13048ec6957446cdf5bb3c8da9e9d4ebc7040e62
Ry-leigh <ryanaarondc@gmail.com>
2025-10-24 15:28:52 +0800

    Feature: add role-based announcement routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AnnouncementController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
@@ -18,6 +19,16 @@ Route::middleware(['auth:sanctum', 'role:Administrator'])
 
     Route::get('/dashboard', function () {return response()->json(['message' => 'Welcome, Admin!']);});
 
+    // api/admin/announcements
+    Route::prefix('announcements')->group(function () {
+        Route::get('/create', [AnnouncementController::class, 'create']); // json for create announcement modal form
+        Route::get('/{id}', [AnnouncementController::class, 'show']); // view details of announcement->id == {id}
+        Route::put('/{id}', [AnnouncementController::class, 'update']); // edit details of announcement->id == {id}
+        Route::delete('/{id}', [AnnouncementController::class, 'destroy']); // delete announcement->id == {id}
+        Route::get('/', [AnnouncementController::class, 'index']); // list all announcements
+        Route::post('/', [AnnouncementController::class, 'store']); // create announcement
+    });
+
 });
 
 // api/instructor
@@ -26,6 +37,16 @@ Route::middleware(['auth:sanctum', 'role:Instructor'])
      ->group(function () {
 
     Route::get('/dashboard', function () {return response()->json(['message' => 'Welcome, Mr./Ms.!']);});
+
+    // api/instructor/announcements
+    Route::prefix('announcements')->group(function () {
+        Route::get('/create', [AnnouncementController::class, 'create']); // json for create announcement modal form
+        Route::get('/{id}', [AnnouncementController::class, 'show']); // view details of announcement->id == {id}
+        Route::put('/{id}', [AnnouncementController::class, 'update']); // edit details of announcement->id == {id}
+        Route::delete('/{id}', [AnnouncementController::class, 'destroy']); // delete announcement->id == {id}
+        Route::get('/', [AnnouncementController::class, 'index']); // list all announcements
+        Route::post('/', [AnnouncementController::class, 'store']); // create announcement
+    });
 
 });
 
@@ -45,5 +66,10 @@ Route::middleware(['auth:sanctum', 'role:Student,Officer'])
      ->group(function () {
 
     Route::get('/dashboard', function () {return response()->json(['message' => 'Hello there!']);});
+
+    Route::prefix('announcements')->group(function () {
+        Route::get('/{id}', [AnnouncementController::class, 'show']); // view details of announcement->id == {id}
+        Route::get('/', [AnnouncementController::class, 'index']); // list all announcements
+    });
 
 });
