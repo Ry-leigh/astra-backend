056989a4ad95268362ccaa9f3f46975a96c6e4a6
Ry-leigh <ryanaarondc@gmail.com>
2025-10-25 14:22:14 +0800

    Feature: defined CRUD methods in ProgramController
    - index(), store(), update() and delete()

--- app/Http/Controllers/ProgramController.php
+++ app/Http/Controllers/ProgramController.php
@@ -2,23 +2,52 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Program;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ProgramController extends Controller
 {
     public function index() {
+        $programs = Program::all();
 
+        return response()->json(['success' => true, 'data' => $programs]);
     }
 
-    public function store() {
+    public function store(Request $request) {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|unique:programs,name',
+            'description' => 'nullable|string',
+        ]);
 
+        if ($validator->fails()) {
+            return response()->json(['success' => false, 'errors' => $validator->errors()], 422);
+        }
+
+        $program = Program::create($request->only(['name', 'description']));
+
+        return response()->json(['success' => true, 'data' => $program], 201);
     }
 
-    public function update() {
+    public function update(Request $request, Program $program)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|unique:programs,name,' . $program->id,
+            'description' => 'nullable|string',
+        ]);
 
+        if ($validator->fails()) {
+            return response()->json(['success' => false, 'errors' => $validator->errors()], 422);
+        }
+
+        $program->update($request->only(['name', 'description']));
+
+        return response()->json(['success' => true, 'data' => $program]);
     }
 
-    public function destroy() {
-        
+    public function destroy(Program $program) {
+        $program->delete();
+
+        return response()->json(['success' => true, 'message' => 'Program deleted successfully']);
     }
 }
