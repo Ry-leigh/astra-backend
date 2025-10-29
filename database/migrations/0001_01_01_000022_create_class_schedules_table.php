d712b489942368f9537375d6ca897b6a8251b5d2
Ry-leigh <ryanaarondc@gmail.com>
2025-10-29 10:54:47 +0800

    Migrations: replaced class_sessions with class_schedules

--- database/migrations/0001_01_01_000022_create_class_schedules_table.php
+++ database/migrations/0001_01_01_000022_create_class_schedules_table.php
@@ -8,13 +8,13 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::create('class_sessions', function (Blueprint $table) {
+        Schema::create('class_schedules', function (Blueprint $table) {
             $table->id();
             $table->foreignId('class_course_id')->constrained('class_courses')->cascadeOnDelete();
-            $table->dateTime('start_datetime');
-            $table->dateTime('end_datetime');
-            $table->enum('status', ['scheduled', 'ongoing', 'completed', 'cancelled'])->default('scheduled');
-            $table->string('topic')->nullable();
+            $table->enum('day_of_week', ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']);
+            $table->time('start_time');
+            $table->time('end_time');
+            $table->boolean('cancelled')->default(false);
             $table->timestamps();
         });
     }
